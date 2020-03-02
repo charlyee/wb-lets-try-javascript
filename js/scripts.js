547b3388c5620ae17c954d19b34f3ff56900e44d
Warren Uhrich <hello@warren.codes>
2020-03-02 11:07:53 -0700

    Console.log practice; If practice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-alert('hello world!');
+// Single line comment.
+
+/*
+ This comment can be:
+ many lines!
+ */
+
+// Camel-casing is common for JavaScript!
+var myInteger = 34; 
+
+// We can output to our console via the console.log method.
+console.log( myInteger * 3 ); 
+
+if ( myInteger > 5 ) {
+    console.log( 'Number is greater than 5.' );
+} else if ( myInteger < 5 ) {
+    console.log( 'Number is less than 5.' );
+} else if ( ( myInteger === 7 ) || ( myInteger === 34 )  ) {
+    console.log( 'Number is... 7 OR 34!?' );
+} else {
+    console.log( 'Number is 5!?' );
+}
