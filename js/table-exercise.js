883dbf48a054cba5b4b9d2d6197ce675a5985d40
Warren Uhrich <hello@warren.codes>
2020-03-04 11:29:36 -0700

    Added delete buttons; added delete function.

--- js/table-exercise.js
+++ js/table-exercise.js
@@ -3,6 +3,25 @@
 // First capture the form!
 var myForm = document.getElementById( 'table-form' );
 
+/**
+ * Delete table row element (assuming we click our 'tr > td > button' element.)
+ */
+function deleteRow( element ) { // Element will be the button we pressed.
+    var tableRow = element.parentNode.parentNode; // 2 levels up from our button, to get our row.
+    tableRow.parentNode.removeChild( tableRow ); // An element cannot delete itself... so we have to tell the parent to do it for us!
+}
+
+// Grab existing buttons.
+var allButtons = document.querySelectorAll( 'td > button' );
+// Loop through our query selected buttons.
+for ( var i = 0; i < allButtons.length; i++ ) {
+    // Let's add listeners to these!
+    allButtons[i].addEventListener( 'click', function ( event ) {
+        deleteRow( this );
+    } );
+}
+
+
 // Listen for a form submission...
 myForm.addEventListener( 'submit', function ( event ) {
     // Prevent the form from ACTUALLY submitting (would leave or refresh the page, terminating our script.)
@@ -40,6 +59,13 @@ myForm.addEventListener( 'submit', function ( event ) {
     var cityCell = document.createElement( 'TD' );
     cityCell.textContent = cityValue;
     newRow.appendChild( cityCell );
+
+    // Create our delete button.
+    var deleteButton = document.createElement( 'BUTTON' );
+    deleteButton.textContent = 'Delete Row'; // Add some text.
+    var actionCell = document.createElement( 'TD' ); // Create the actions cell!
+    actionCell.appendChild( deleteButton ); // Add the button inside.
+    newRow.appendChild( actionCell ); // Don't forget to add this new cell to the row too!
 
     // Target your table body.
     var tableBody = document.getElementById( 'table-body' );
